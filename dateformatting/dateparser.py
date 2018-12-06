30ea0aa125a6288e87cdb69a06a7397479c1feca
Erik van der Maden <Erik@conneqtech.com>
2018-12-06 13:46:43 +0100

    additional comments, changed date prediction and got rid of page amount hardcoding

--- dateformatting/dateparser.py
+++ dateformatting/dateparser.py
@@ -5,46 +5,36 @@ from datetime import *
 import dateparser
 
 tree = ET.parse('dateformatting/date_rules.xml')
-root = tree.getroot()
 blacklist = tree.find('blacklisted_phrases')
 unparsable_phrases = tree.find('unparsable_phrases')
 
-years_passed = 0
 
-
-def parse(date: str) -> datetime:
-    parsable_date = remove_unparsable_phrases(date)
+def parse(date_to_parse: str) -> datetime:
+    parsable_date = remove_unparsable_phrases(date_to_parse)
     parsed_date = dateparser.parse(parsable_date)
 
     return parsed_date
 
 
-def is_blacklisted(date: str):
+def is_blacklisted(date_to_check: str):
     for phrase in blacklist:
-        if re.search(phrase.text, str(date), re.IGNORECASE):
+        if re.search(phrase.text, str(date_to_check), re.IGNORECASE):
             return True
     return False
 
 
-def remove_unparsable_phrases(date: str):
+def remove_unparsable_phrases(date_to_edit: str):
     for phrase in unparsable_phrases:
         pattern = re.compile(phrase.text, re.IGNORECASE)
-        pattern.sub("", str(date))
-    return str(date)
+        pattern.sub("", str(date_to_edit))
+    return str(date_to_edit)
 
 
-""" 
-This function guesses the year a date should take place in based on
-a known date that occurs within a year of the date that will be guessed
-it returns the guessed date.
-"""
+def change_date_to_future(date_to_change: datetime) -> datetime:
+    """" If a date is set in the past this method will change it to the next time that day + month
+    is set in the future
 
-
-def guess_year(known_date: datetime, date_to_guess: datetime) -> datetime:
-    # date_to_guess = date_to_guess.replace(year=known_date.year)
-
-    # date to guess is always in the same year as known year, or in the year after
-    if date_to_guess < known_date:
-        date_to_guess = date_to_guess.replace(year=(known_date.year + 1))
-
-    return date_to_guess
+    e.g. if today is six december 2018  and date is the first of january 2018 it will be changed to
+    the first of january 2019"""
+    return date_to_change.replace(year=(date_to_change.year + 1)) if date_to_change < datetime.now() \
+        else date_to_change
--- scraper.py
+++ scraper.py
@@ -4,17 +4,23 @@ from python_objects.ScrapeBlueprint import EventBlueprint
 import dateformatting.dateparser as dp
 import urllib3
 import time
-import bs4
+import bs4 as bs
 import csv
 
-# retrieve al attribute definitions from xml file
+# retrieve all attribute definitions from xml file
 blueprint = EventBlueprint('scrape_definitions/vvv_zeeland.xml')
+
+# flag gets set to true once pages no longer have events on them
+scraped_all_pages = False
 
 
 def find_value_in_soup(soup, html_class, position):
     value = ''
+
+    #  finds all values with matching html class
     possible_values = list(soup.findAll(attrs={'class': html_class}, recursive=True))
 
+    # checks if there are enough results to grab the one at the position you want
     if len(possible_values) > position:
         value = possible_values[position].get_text().strip()
 
@@ -26,23 +32,31 @@ def get_events_from_page(page_url):
     page_data = get_page_data(page_url)
 
     # makes a soup of all event containers
-    event_soup = bs4.BeautifulSoup(page_data, features="lxml",
-                                   parse_only=bs4.SoupStrainer(class_=blueprint.event_class))
+    event_soup = bs.BeautifulSoup(page_data, features="lxml", parse_only=bs.SoupStrainer(class_=blueprint.event_class))
+
     # list that will be filled with events as they get processed
     processed_events = []
+
+    # checks if there are still events (event_soup will always contain at least 1 item)
+    if len(list(event_soup)) == 1:
+        global scraped_all_pages
+        scraped_all_pages = True
+        return processed_events
 
     # repeat for every event container
     for event in list(event_soup):
 
         # makes a soup for a single event
-        event_soup = bs4.BeautifulSoup(str(event), features="lxml")
+        event_soup = bs.BeautifulSoup(str(event), features="lxml")
 
         name = find_value_in_soup(event_soup, blueprint.name_class, blueprint.name_position)
         date = find_value_in_soup(event_soup, blueprint.date_class, blueprint.date_position)
         location = find_value_in_soup(event_soup, blueprint.location_class, blueprint.location_position)
 
         # checks if the data is fit to be saved.
-        if name is not None and location is not None and date is not None and not dp.is_blacklisted(date):
+        if not None in (name, location, date) and not dp.is_blacklisted(date):
+            date = dp.parse(date)
+            date = dp.change_date_to_future(date)
             processed_events.append(Event(name, dp.parse(date), location))
 
     return processed_events
@@ -52,14 +66,13 @@ def get_events_from_page(page_url):
 def scrape_all(url, page_indicator, time_out_time):
     full_event_list = []
 
-    x = 1
-    number_of_pages = 5
-    while x != number_of_pages:
-        new_url = url + page_indicator + str(x)
+    page = 1
+    while not scraped_all_pages:
+        new_url = url + page_indicator + str(page)
 
         page_events = get_events_from_page(new_url)
         full_event_list += page_events
-        x += 1
+        page += 1
         print(new_url)
 
         # sleep the thread to prevent sending to many requests in to short of a time
@@ -73,7 +86,8 @@ def scrape_all(url, page_indicator, time_out_time):
 # sends a request to url parameter and returns html result as a string
 def get_page_data(url):
     hdr = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+        'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
         'Accept-Encoding': 'none',
@@ -84,7 +98,7 @@ def get_page_data(url):
     return html_string.data
 
 
-# # Scrape from VVV Zeeland
+# Scrape from VVV Zeeland
 event_list = scrape_all(blueprint.url, blueprint.url_page_indicator, 1)
 
 # Write events to csv file
